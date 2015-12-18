b131b66649b407a101df12ffddb4a177aed11d47
Charles Randles <charles.randles@gmail.com>
2015-12-18 23:57:50 +1000

    Midnight is a good time to stop.

--- show_lama.py
+++ show_lama.py
@@ -20,7 +20,7 @@ form = cgi.FieldStorage()
 lamaId = form['id'].value
 sql = "select * from lama where id=?"
 cursor = database.getCursor().execute(sql, lamaId)
-html += cursor.fetchone()
+html += str(cursor.fetchone())
 html += """
   </body>
 </html>  
